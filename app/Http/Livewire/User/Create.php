952269a6534bad22ac87e45f20dc008ce3dbecb0
Ian Kollipara <ian.kollipara@gmail.com>
2024-05-07 17:53:52 -0500

    Update package name in package-lock.json and remove unnecessary properties in Create.php

--- app/Http/Livewire/User/Create.php
+++ app/Http/Livewire/User/Create.php
@@ -18,31 +18,19 @@ class Create extends Component
     /** @var UploadedFile|null */
     public $avatar = null;
 
-    public string $password = "";
-    public string $password_confirmation = "";
-    public string $full_name = "";
     public string $bio = '{"blocks": []}';
-    public bool $above_19 = false;
 
     /** @var string[]|array<string, array<string>> */
     protected $rules = [
         "user.first_name" => "required|string",
         "user.last_name" => "required|string",
         "user.consented" => "boolean|nullable",
-        "above_19" => "required|boolean",
-        "full_name" => "string|required",
         "user.email" => "required|email|unique:users,email",
-        "password" => [
-            "string",
-            'regex:/^(?=.*[1-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()]).{12,}$/',
-            "required",
-        ],
         "avatar" => ["image", "nullable"],
         "bio" => ["json"],
         "user.grades" => ["array", "required"],
         "user.subject" => ["required"],
         "user.school" => ["required_unless:user.is_preservice,true"],
-        "password_confirmation" => ["same:password", "required"],
         "user.years_of_experience" => [
             "integer",
             "min:0",
@@ -51,16 +39,10 @@ class Create extends Component
         "user.is_preservice" => ["required", "boolean"],
     ];
 
-    /** @var string[] */
-    protected $messages = [
-        "password.regex" =>
-            'The password needs at least 1 lowercase, one uppercase, one number, one symbol (@#$%^&-+=()), and is at least 12 characters.',
-        "password_confirmation.same" => "Must match the password",
-    ];
-
     public function mount(): void
     {
         $this->user = new User();
+        $this->user->consented = true;
     }
 
     /**
@@ -70,13 +52,6 @@ class Create extends Component
     public function updated($propertyName, $value): void
     {
         $this->validateOnly($propertyName);
-        if ($propertyName === "full_name") {
-            ($this->user->consented = $value === $this->user->full_name()) and
-                $this->above_19;
-        } elseif ($propertyName === "above_19") {
-            ($this->user->consented =
-                $this->full_name === $this->user->full_name()) and $value;
-        }
     }
 
     public function updatedBio(): void
@@ -99,10 +74,7 @@ class Create extends Component
                 ),
             ]);
         }
-        $this->user->fill(["password" => $this->password]);
-        if ($this->above_19 and $this->full_name === $this->user->full_name()) {
-            $this->user->consented = true;
-        }
+        $this->user->fill(["password" => ""]);
         $this->dispatchBrowserEventIf(!$this->user->save(), "error", [
             "message" => __(
                 "There was an error signing up." .
--- resources/views/livewire/user/create.blade.php
+++ resources/views/livewire/user/create.blade.php
@@ -2,7 +2,7 @@
   <x-hero class="is-primary">
     <h1 class="title">Sign Up</h1>
   </x-hero>
-  <main x-data="{ currentStep: 0 }" class="container is-fluid mt-5">
+  <main x-data="{ currentStep: 0, isPreservice: @entangle('user.is_preservice').defer}" class="container is-fluid mt-5">
     <form wire:submit.prevent='save' class="mt-5 mb-5" enctype="multipart/form-data" method="post">
       <x-forms.step step="0" current-step="currentStep">
         <h2 class="subtitle is-3 has-text-centered">Personal Information</h2>
@@ -10,7 +10,7 @@
         <x-forms.input label="Last Name" name="last_name" wire:model.debounce.200ms="user.last_name" />
         <x-forms.input label="Email" name="email" type="email" wire:model.debounce.200ms="user.email" />
       </x-forms.step>
-      <x-forms.step step="1" current-step="currentStep">
+      <x-forms.step step="1" current-step="currentStep" >
         <h2 class="subtitle is-3 has-text-centered">Teacher Information</h2>
         <div class="field">
           <label for="grades" class="label">Grades</label>
@@ -21,14 +21,14 @@
           </div>
         </div>
         <label class="checkbox">
-          <input type="checkbox" wire:model='user.is_preservice'>
+          <input type="checkbox" x-model="isPreservice">
           I am a preservice teacher
         </label>
-        @if ($this->user->is_preservice === false)
-          <x-forms.input label="School" name="school" wire:model.debounce.200ms="user.school" />
-          <x-forms.input label="Years of Experience" name="years_of_experience" type="number"
-            wire:model.debounce.200ms="user.years_of_experience" />
-        @endif
+          <span x-show="!isPreservice"><x-forms.input label="School" name="school" wire:model.debounce.200ms="user.school" /></span>
+          <span x-show="!isPreservice">
+              <x-forms.input label="Years of Experience" name="years_of_experience" type="number"
+                wire:model.debounce.200ms="user.years_of_experience" />
+          </span>
         <x-forms.input label="Subject" name="subject" wire:model.debounce.200ms="user.subject" />
       </x-forms.step>
       <x-forms.step step="2" current-step="currentStep">
@@ -38,30 +38,7 @@
         </div>
         <x-editor name="bio" wire:model="bio" cannot-upload />
       </x-forms.step>
-      <x-forms.step step="3" current-step="currentStep">
-        <h2 class="subtitle is-3 has-text-centered">Your Password</h2>
-        <x-forms.password label="Password" name="password" wire:model.debounce.200ms="password" />
-        <ul style="list-style: circle; list-style-position: inside;" class="column is-fullwidth">
-          <x-forms.rule rule="^(?=.*[0-9])" model="$wire.password">
-            Must contain at least one digit
-          </x-forms.rule>
-          <x-forms.rule rule="^(?=.*[a-z])" model="$wire.password">
-            Must contain at least one lowercase letter
-          </x-forms.rule>
-          <x-forms.rule rule="^(?=.*[A-Z])" model="$wire.password">
-            Must contain at least one uppercase letter
-          </x-forms.rule>
-          <x-forms.rule rule="^(?=.*[@#$%^&-+=()])" model="$wire.password">
-            Must contain at least one special character (e.g. @#$%^&-+=())
-          </x-forms.rule>
-          <x-forms.rule rule=".{12,}" model="$wire.password">
-            Must be at least 12 characters long
-          </x-forms.rule>
-        </ul>
-        <x-forms.password label="Confirm Password" name="password_confirmation"
-          wire:model.debounce.200ms="password_confirmation" />
-      </x-forms.step>
-      <section x-data='{checked: true}' x-show="currentStep === 4">
+      <section x-data='{checked: @entangle('user.consented').defer, above: false, fullName: ""}' x-show="currentStep === 3">
         <h2 class="subtitle is-3 has-text-centered">conneCTION Research Study</h2>
         <x-research.consent-form>
           <div class="field">
@@ -72,21 +49,21 @@
           </div>
           <div class="field">
             <label class="checkbox" x-bind:class="{ 'is-hidden': !checked }">
-              <input type="checkbox" wire:model='above_19'>
+              <input type="checkbox" x-model="above">
               I am 19 years or older
             </label>
           </div>
-          @if ($this->above_19)
-            <x-forms.input label="Please enter your full name to consent." wire:model.debounce.200ms="full_name"
-              name="full_name" />
-          @endif
+            <span x-show="above">
+                <x-forms.input label="Please enter your full name to consent." x-model="fullName"
+                  name="full_name" />
+            </span>
         </x-research.consent-form>
         <div class="field is-group is-grouped-centered">
           <div class="control">
             <button type="button" x-on:click="currentStep--" class="button is-primary is-outlined">
               Back
             </button>
-            <button x-bind:disabled="checked && !$wire.user.consented" type="submit" class="button is-primary">
+            <button x-bind:disabled="checked && !above && fullName===''" type="submit" class="button is-primary">
               Submit
             </button>
           </div>
