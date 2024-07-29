e5c0aa7456498a0b77d3c4c1ed59bf380934c976
Ian Kollipara <ian.kollipara@gmail.com>
2024-07-29 13:03:16 -0500

    Removed all mix and migrated fully to vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import compression from "vite-plugin-compression2";
 
 export default defineConfig({
   plugins: [
@@ -9,5 +10,9 @@ export default defineConfig({
       "resources/css/animate.css",
       "resources/scss/app.scss",
     ]),
+    compression({
+      algorithm: "brotliCompress",
+      filename: "[path][base].br",
+    }),
   ],
 });
