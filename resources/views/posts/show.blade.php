96f568a0a9768454c7d5938893ae5eef51c74513
Ian Kollipara <ian.kollipara@gmail.com>
2024-09-25 06:25:53 -0500

    Refactor show.blade.php to include post body in a script tag

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,13 +6,18 @@ description: Show view for a post
  --}}
 
 <x-reading-layout title="{{ $post->title }}">
+  @push('scripts')
+    <script>
+      window.body = '{!! $post->body->toJson() !!}';
+    </script>
+  @endpush
   <x-slot:aside>
     @include('posts.partials.details-sidebar', ['post' => $post])
   </x-slot>
   <main class="flex flex-col gap-y-3">
     <x-title>{{ $post->title }}</x-title>
     @include('posts.partials.action-bar', ['post' => $post])
-    <div x-data="editor({ name: 'name', readOnly: true, canUpload: true, csrf: '{{ csrf_token() }}', body: '{!! $post->body->toJson() !!}' })">
+    <div x-data="editor({ name: 'name', readOnly: true, canUpload: true, csrf: '{{ csrf_token() }}', body: window.body })">
       <input name="name"
              type="hidden"
              x-bind="input">
