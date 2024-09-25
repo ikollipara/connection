0445b24e7ea56ef48e0a6c1de395ef8acd05a42b
Ian Kollipara <ian.kollipara@gmail.com>
2024-09-25 07:04:27 -0500

    Refactor JavaScript imports and remove unused code

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -8,67 +8,57 @@
  *------------------------------------------------------------**/
 
 import { Calendar } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listViewPlugin from "@fullcalendar/list";
+import iCalPlugin from "@fullcalendar/icalendar";
 
 export default ({ user = null }) => ({
   init() {
-    Promise.allSettled([
-      import("@fullcalendar/daygrid"),
-      import("@fullcalendar/timegrid"),
-      import("@fullcalendar/list"),
-      import("@fullcalendar/icalendar"),
-    ]).then(
-      ([
-        { default: dayGridPlugin },
-        { default: timeGridPlugin },
-        { default: listViewPlugin },
-        { default: iCalPlugin },
-      ]) => {
-        this.calendar = new Calendar(this.$el, {
-          plugins: [dayGridPlugin, timeGridPlugin, iCalPlugin, listViewPlugin],
-          initialView: window.innerWidth > 768 ? "dayGridMonth" : "listWeek",
-          headerToolbar:
-            window.innerWidth > 768
-              ? {
-                  left: "prev,next today",
-                  center: "title",
-                  right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
-                }
-              : {
-                  left: "prev",
-                  center: "title",
-                  right: "next",
-                },
-          height: "80dvh",
-          editable: false,
-          selectable: true,
-          handleWindowResize: true,
-          windowResize: ({ view }) => {
-            if (window.innerWidth < 768) {
-              view.calendar.changeView("listWeek");
-              view.calendar.setOption("height", "auto");
-              view.calendar.setOption("headerToolbar", {
-                left: "prev",
-                center: "title",
-                right: "next",
-              });
-            } else {
-              view.calendar.changeView("dayGridMonth");
-              view.calendar.setOption("height", "80dvh");
-              view.calendar.setOption("headerToolbar", {
-                left: "prev,next today",
-                center: "title",
-                right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
-              });
+    this.calendar = new Calendar(this.$el, {
+      plugins: [dayGridPlugin, timeGridPlugin, iCalPlugin, listViewPlugin],
+      initialView: window.innerWidth > 768 ? "dayGridMonth" : "listWeek",
+      headerToolbar:
+        window.innerWidth > 768
+          ? {
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
             }
-          },
-          events: {
-            format: "ics",
-            url: route("events.ical", { user }),
-          },
-        });
-        this.$nextTick(() => this.calendar.render());
+          : {
+              left: "prev",
+              center: "title",
+              right: "next",
+            },
+      height: "80dvh",
+      editable: false,
+      selectable: true,
+      handleWindowResize: true,
+      windowResize: ({ view }) => {
+        if (window.innerWidth < 768) {
+          view.calendar.changeView("listWeek");
+          view.calendar.setOption("height", "auto");
+          view.calendar.setOption("headerToolbar", {
+            left: "prev",
+            center: "title",
+            right: "next",
+          });
+        } else {
+          view.calendar.changeView("dayGridMonth");
+          view.calendar.setOption("height", "80dvh");
+          view.calendar.setOption("headerToolbar", {
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+          });
+        }
       },
-    );
+      events: {
+        format: "ics",
+        url: route("events.ical", { user }),
+      },
+    });
+    this.$nextTick(() => this.calendar.render());
   },
   destroy() {
     this.calendar.destroy();
--- resources/views/components/form-rich-text.blade.php
+++ resources/views/components/form-rich-text.blade.php
@@ -5,17 +5,28 @@ date: 2024-09-09
 description: The HTML for the rich text form component
  --}}
 
-@props(['name', 'cannotUpload' => false, 'value' => '{"blocks": []}'])
+@props(['name', 'cannotUpload' => false, 'value' => null])
 
 @aware(['model', 'formName'])
 
 @php
-  $value = old($name, $model?->getAttribute($name)?->toJson() ?? $value);
+  $value ??= Js::from(['blocks' => []]);
+  $old = old($name)
+  if($old) {
+    $value = Js::from($old);
+  } elseif($model?->getAttribute($name)) {
+    $value = Js::from($model?->getAttribute($name));
+  } else {
+    $value = Js::from($value);
+  }
 @endphp
 
+<script>
+  window.body = JSON.stringify({{ $value }});
+</script>
 <div {{ $attributes }}
      x-modelable="body"
-     x-data="editor({ name: @js($name), readOnly: false, canUpload: @js(!$cannotUpload), csrf: '{{ csrf_token() }}', body: '{{ $value }}' })">
+     x-data="editor({ name: @js($name), readOnly: false, canUpload: @js(!$cannotUpload), csrf: '{{ csrf_token() }}', body: window.body })">
   <input name="{{ $name }}"
          type="hidden"
          x-bind="input">
