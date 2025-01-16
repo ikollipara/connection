09ee82ad953c498e7da0d18ef9c378c89460bbb2
Ian Kollipara <ian.kollipara@gmail.com>
2025-01-16 16:24:58 -0600

    Added database path

--- rector.php
+++ rector.php
@@ -5,7 +5,8 @@ use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__.'/app',
+        __DIR__ . '/app',
+        __DIR__ . '/database',
     ])
     ->withRules([
         DeclareStrictTypesRector::class,
