8ba016a1d947a3bc949ce036f582dcf16216fc8f
Ian Kollipara <ian.kollipara@gmail.com>
2025-01-16 16:16:05 -0600

    Added Rector

---
+++ rector.php
@@ -0,0 +1,13 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+    ])
+    ->withRules([
+        DeclareStrictTypesRector::class
+    ]);
